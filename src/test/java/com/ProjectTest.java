2bb176d3d3bd4f48a3dc743a898c95d1f3835ef8
shenzhiqiang <shenzq89@hotmail.com>
2016-09-27 11:17:38 +0800

    update test

---
+++ src/test/java/com/ProjectTest.java
@@ -0,0 +1,32 @@
+package com;
+
+import com.lanxi.PointsExchangeAccountCheck.dao.ChkBrchMapper;
+import com.lanxi.PointsExchangeAccountCheck.dao.ChkErrMapper;
+import com.lanxi.PointsExchangeAccountCheck.dao.ChkPltMapper;
+import com.lanxi.PointsExchangeAccountCheck.entity.db.AccountCheckCtrlBean;
+import com.lanxi.PointsExchangeAccountCheck.service.AccountCheckServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by shenzhiqiang on 16-9-23.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class ProjectTest {
+
+
+
+    @Test
+    public void test1() {
+
+
+
+    }
+
+
+}
